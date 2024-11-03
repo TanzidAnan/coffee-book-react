b68bfd614fea9e36ae561f9438acafe2f514d378
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-03 13:47:30 +0600

    navber section style

--- src/Compontents/NavBar.jsx
+++ src/Compontents/NavBar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+
 
 const NavBar = () => {
     return (
@@ -23,36 +24,20 @@ const NavBar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            <Link to='/'><li><a>Home</a></li></Link>
+                            <Link to='/coffees'><li><a>Coffees</a></li></Link>
+                            <Link to='/dashboard'><li><a>Dashboard</a></li></Link>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link to='/' className=" text-xl">Cook_Book</Link>
                 </div>
-                <div className="navbar-center hidden lg:flex">
+                <div className="navbar-end hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <Link to='/'><li><a>Home</a></li></Link>
+                        <Link to='/coffees'><li><a>Coffees</a></li></Link>
+                        <Link to='/dashboard'><li><a>Dashboard</a></li></Link>
+
                     </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
                 </div>
             </div>
         </div>
