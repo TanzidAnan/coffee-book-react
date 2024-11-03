e32dacc3843825de9d70592586749d0ca9b36203
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-03 17:14:59 +0600

    categories data

--- src/Compontents/CoffeeCard.jsx
+++ src/Compontents/CoffeeCard.jsx
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom";
 
 const CoffeeCard = () => {
+    const {tanzid} = useParams();
+    // console.log(obj)
+    // console.log(obj)
     return (
         <div>
-            <h1>coffee card</h1>
+            <h1>coffee card ..... {tanzid}</h1>
         </div>
     );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -15,10 +15,10 @@ const routes =createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>,
-                loader:() => fetch('./categories.json'),
+                loader:() => fetch('../categories.json'),
                 children:[
                     {
-                        path:'/categorie/:categorie',
+                        path:'/categorie/:tanzid',
                         element:<CoffeeCard></CoffeeCard>
                     }
                 ]
