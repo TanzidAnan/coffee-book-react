0652b8c3edd7fc4fd4d6dd62a304d023e07cfd2f
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-03 11:43:53 +0600

    navber section

---
+++ src/Pages/Coffess.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coffess = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Coffess;
---
+++ src/Pages/CoffessDetles.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoffessDetles = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CoffessDetles;
