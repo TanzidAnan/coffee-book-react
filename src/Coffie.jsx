2fed23cf7837c7a59f9a8ce4935b356f16a95452
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-23 22:13:55 +0600

    detles data

--- src/Coffie.jsx
+++ src/Coffie.jsx
@@ -1,13 +1,18 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
 const Coffie = () => {
-    const data =useLoaderData();
-    const {id} =useParams();
-    const detlesId =data.find(coffes => coffes.id === parseInt(id))
+    const data = useLoaderData();
+    const { id } = useParams();
+    const detlesId = data.find(coffes => coffes.id === parseInt(id))
     console.log(detlesId)
     return (
         <div className="flex justify-center items-center">
-            <h1>{detlesId.category}</h1>
+            <div>
+                <h1>{detlesId.category}</h1>
+               <div>
+               <img src={detlesId.image} alt="" />
+               </div>
+            </div>
         </div>
     );
 };
