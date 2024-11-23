b9e77caca6cee1a60647129a55bed30e103a5ace
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-23 22:08:24 +0600

    data

--- src/Coffie.jsx
+++ src/Coffie.jsx
@@ -2,9 +2,8 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 const Coffie = () => {
     const data =useLoaderData();
-    console.log(data)
     const {id} =useParams();
-    console.log(id)
+    
     return (
         <div>
             <h1>tanizd</h1>
