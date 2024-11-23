6d58092eaf4ff36f58bdb04d652057e6bee2215d
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-23 22:38:14 +0600

    data

--- src/Coffie.jsx
+++ src/Coffie.jsx
@@ -12,7 +12,7 @@ const Coffie = () => {
                 <div >
                     <img className="h-[60vh] w-full rounded-2xl" src={detlesId.image} alt="" />
                 </div>
-                <div className="my-7 border-2">
+                <div className="my-7 border-2 py-3 px-2">
                     <p>{detlesId.description}</p>
                 </div>
             </div>
