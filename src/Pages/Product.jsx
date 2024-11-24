a10a83cd0d8c8ca2d5c3352f96bb66f85b76006f
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-24 23:54:59 +0600

    card

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,23 @@
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
     console.log(product)
+    co
     return (
         <div>
-            
+            <div className="card card-compact bg-base-100 w-96 shadow-xl">
+                <figure className="h-[30vh] w-full">
+                    <img
+                        className="h-full w-full"
+                        src={image}
+                        alt="coffees" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <h2 className="card-title">categry: {category}</h2>
+                    <p>type: {type}</p>
+                    <p>type: {rating}</p>
+                </div>
+            </div>
         </div>
     );
 };
